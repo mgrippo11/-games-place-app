68c9d58da817785fb318b36e5eea11af10930d6a
mgrippo11 <mgrippo_11@hotmail.com>
2023-02-13 17:55:47 -0300

    Add Pokemon page

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,7 +1,65 @@
+import { useState } from "react"
+
+const POKEMONS = [
+    "bulbasaur",
+    "ivysaur",
+    "venusaur",
+    "charmander",
+    "charmeleon",
+    "charizard",
+    "squirtle",
+    "wartortle",
+    "blastoise",
+    "caterpie",
+    "metapod",
+    "butterfree",
+    "weedle",
+    "kakuna",
+    "beedrill",
+    "pidgey",
+    "pidgeotto",
+    "pidgeot",
+    "rattata",
+    "raticate",
+    "spearow",
+    "fearow",
+    "ekans",
+    "arbok",
+    "pikachu",
+    "raichu",
+    "sandshrew",
+    "sandslash"
+]
+
+const POKEID = Math.floor(Math.random() * POKEMONS.length)
+
 export default function Pokemon() {
+  const [hasWon, setHasWon] = useState(false)
+  const [buffer, setBuffer] = useState("")
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    if (buffer.toLocaleLowerCase() === POKEMONS[POKEID]) {
+      setHasWon(true)
+      console.log("Correcto Ganaste!")
+    } else {
+      console.log("Incorrecto! Volve a intentar...")
+    }
+  }
+
   return (
     <div>
-      Pokemon
+      <img className="imgPokemon" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${POKEID + 1}.png`} alt="pokemon " style={{filter: hasWon ? "" : "brightness(0) invert(1)"}}/>
+      {hasWon ? (
+          <button onClick={() => location.reload()}>Jugar de nuevo</button>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <input type="text" autoFocus onChange={(e) => setBuffer(e.target.value)}/>
+            <button type="submit">Submint</button>
+          </form>
+        )
+      }
     </div>
   )
 }
--- src/pages/WordsPerMinute.jsx
+++ src/pages/WordsPerMinute.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const WORDS = [
   "Azul",
@@ -24,11 +24,43 @@ const WORDS = [
 ]
 
 export default function WordsPerMinute() {
+  const [word, setWord] = useState(() => WORDS[(Math.random() * WORDS.lengh) | 0])
   const [CharacterCount, setCharacterCount] = useState(0)
+  const [buffer, setBuffer] = useState("")
+  const [time, setTime] = useState(0)
 
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    if (buffer === word) {
+      setWord(WORDS[(Math.random() * WORDS.length) | 0])
+      setCharacterCount((CharacterCount) => CharacterCount + word.length)
+    }
+    setBuffer("")
+  }
+
+    useEffect(() => {
+      if (time !== 0) {
+        const timeout = setTimeout(() => setTime(time -1), 1000)
+
+        return () => clearTimeout(timeout)
+      }
+    }, [time])
+  
   return (
-    <div>
-      <h1>Caracteres tipeados: {CharacterCount}</h1>
+    <div className="wpmDiv">
+      { Boolean(time) && <h1>{word}</h1> }
+      <h3>Caracteres tipeados: {CharacterCount}</h3>
+      <h3>Tiempo restante: {time}</h3>
+      {time ? (
+        <form onSubmit={handleSubmit}>
+          <input type="text" autoFocus value={buffer} onChange={(e) => setBuffer(e.target.value)}/>
+          <button type="submit">submit</button>
+        </form>
+      ) : (
+        <button onClick={() => setTime(60)}>Play</button>
+      )
+      }
     </div>
   )
 }
